62f4aad3b642ade67da8ef8ad01a332233157037
Neal Howard <nhoward72@yahoo.com>
2020-04-10 15:06:43 -0500

    Add project files.

---
+++ EF_Blogs_NHoward/Models/BloggingContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace BlogsConsole.Models
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext() : base("name=BlogContext") { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public void AddBlog(Blog blog)
+        {
+            this.Blogs.Add(blog);
+            this.SaveChanges();
+        }
+        public void AddPost(Post post)
+        {
+            this.Posts.Add(post);
+            this.SaveChanges();
+        }
+        public void ListBlogs()
+        {
+            var query = Blogs.OrderBy(b => b.Name);
+
+            Console.WriteLine("All blogs in the database:");
+            Console.WriteLine("{0,-10}{1,-20}", "Blog Id", "Blog Name");
+            Console.WriteLine("-------------------------------------");
+            foreach (var item in query)
+            {
+                Console.WriteLine("{0,-10}{1,-20}", item.BlogId, item.Name);
+                Console.WriteLine();
+
+            }
+        }
+        public void MakeNewPost()
+        {
+            string blogID,title, postData;
+            Console.WriteLine("Which blog would you like to post to?");
+            ListBlogs();
+            blogID = Console.ReadLine();
+            Post post = new Post();
+            Console.WriteLine("Title of Post:");
+            title = Console.ReadLine();
+            Console.WriteLine("Post Content:");
+            postData = Console.ReadLine();
+            post.Title = title;
+            post.Content = postData;
+            post.BlogId = Int32.Parse(blogID);
+            AddPost(post);
+        }
+    }
+}
---
+++ EF_Blogs_NHoward/Program.cs
@@ -0,0 +1,52 @@
+﻿using NLog;
+using BlogsConsole.Models;
+using System;
+using System.Linq;
+
+namespace BlogsConsole
+{
+    class MainClass
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public static void Main(string[] args)
+        {
+            string keyboard;
+            logger.Info("Program started");
+            Console.WriteLine("Welcome to the Blogger 9000");
+
+            do
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("(1) Create a Blog");
+                Console.WriteLine("(2) List All Blogs");
+                Console.WriteLine("(3) Add a Post");
+                Console.WriteLine("(9) Exit ");
+                keyboard = Console.ReadLine();
+                var db = new BloggingContext();
+                switch (keyboard)
+                {
+                    case "1":
+                        // Create and save a new Blog
+                        Console.Write("Enter a name for a new Blog: ");
+                        var name = Console.ReadLine();
+                        var idNum = (db.Blogs.Count()) + 100; 
+                        var blog = new Blog { BlogId = idNum,
+                                              Name = name };
+
+                        db.AddBlog(blog);
+                        logger.Info("Blog added - {name}", name);
+                        break;
+                    case "2":
+                        db.ListBlogs();
+                        break;
+                    case "3":
+                        db.MakeNewPost();
+                        break;
+                }
+
+
+            } while (keyboard != "9");
+
+        }
+    }
+}
