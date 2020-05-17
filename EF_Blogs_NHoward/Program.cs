79f7af95b79ce0c418f260678a1c5648daad6e3f
Neal Howard <nhoward72@yahoo.com>
2020-05-17 14:49:48 -0500

    Updated Menu

--- EF_Blogs_NHoward/Program.cs
+++ EF_Blogs_NHoward/Program.cs
@@ -22,6 +22,7 @@ namespace BlogsConsole
                 Console.WriteLine("(3) Add a Post");
                 Console.WriteLine("(4) Display a Post");
                 Console.WriteLine("(5) Edit a Post");
+                Console.WriteLine("(6) Delete a Post");
                 Console.WriteLine("(9) Exit ");
                 keyboard = Console.ReadLine();
                 var db = new BloggingContext();
